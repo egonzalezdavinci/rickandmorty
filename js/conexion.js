8c7a823212e5c7638d2ae62171c9f87ba0b6bffa
Elias Gonzalez <elias.gonzalez@davinci.edu.ar>
2022-06-20 20:14:21 -0300

    update funcion
    remover el msj de error luego de 5 segundos

--- js/conexion.js
+++ js/conexion.js
@@ -1,23 +1,32 @@
 let tagBodyMensaje = document.getElementsByTagName('body')[0];
 let contentMessageOffline = document.createElement('div');
 let messageOffline = document.createElement('div');
-	contentMessageOffline.className = 'contentMessage';
 	messageOffline.className = 'alert alert-danger';
 	messageOffline.innerHTML = 'Sin conexión';
 
 window.addEventListener('offline', event => {
     console.log('Estoy Offline!!');
+    contentMessageOffline.className = 'contentMessage';
     tagBodyMensaje.appendChild(contentMessageOffline);
     contentMessageOffline.appendChild(messageOffline);
+    setTimeout(function(){
+        contentMessageOffline.removeChild(messageOffline);
+        tagBodyMensaje.removeChild(contentMessageOffline);
+        },5000);
 })
 window.addEventListener('online', event => {
     console.log('Estoy online!!');
-    messageOffline = '';
+
 });
 
 if (!navigator.onLine){
     console.log('Estoy sin conexion, rey!');
     alert("No estas conectado");
+    contentMessageOffline.className = 'contentMessage';
     tagBodyMensaje.appendChild(contentMessageOffline);
     contentMessageOffline.appendChild(messageOffline);
+    setTimeout(function(){
+        contentMessageOffline.removeChild(messageOffline);
+        tagBodyMensaje.removeChild(contentMessageOffline);
+        },5000);
 }
